e2254ee7a756373c8041537992b22e2d250f5026
stha sujan <sthasujan1374@gmail.com>
2024-09-18 17:07:24 +0545

    screen size config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,26 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        khand: ["Khand", "sans-serif"],
+        khand: ["Khand"],
         mukta: ["Mukta", "serif"],
       },
       fontWeight: {
+        "khand-light": 300,
         "khand-regular": 400,
+        "khand-medium": 500,
+        "khand-semibold": 600,
         "khand-bold": 700,
-        "mukta-regular": 400,
-        "mukta-bold": 700,
+      },
+      screens: {
+        xxs: "380px",
+        mxs: "425px",
+        xs: "475px",
+        xsm: "540px",
+        sm: "670px",
+        md: "800px",
+        lg: "1038px",
+        xl: "1280px",
+        "2xl": "1550px",
       },
     },
   },
